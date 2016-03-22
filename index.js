af547197bccf7a11cced9ef7cdc7a452d473026b
Niv Sardi <xaiki@evilgiggle.com>
2016-03-22 16:03:40 -0300

    port phnz changes
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 var inherits = require('util').inherits
-  , ytdl = require('ytdl');
+  , ytdl = require('ytdl-core');
 
 var Streamer = require('butter-base-streamer');
 
 /* -- YouTube Streamer -- */
 function YoutubeStreamer(source, options) {
-	if(!(this instanceof YoutubeStreamer)) 
+	if(!(this instanceof YoutubeStreamer))
 		return new YoutubeStreamer(source, options);
 
 	Streamer.call(this, options);
 	var self = this;
 	options = options || {};
+	options.youtube = options.youtube || {};
 
 	this._options = options;
 	this._source = source;
-	this._video = ytdl(source, {quality: options.hd ? 22 : 18});
+	this._video = ytdl(source, {quality: options.youtube.audio ? 140 : (options.youtube.hd ? 22 : 18)});
 	this._video.on('info', function(info, format) {
 		self._progress.setLength(format.size);
+        self.file.length = format.size;
 	})
 	this._streamify.resolve(this._video);
 }
@@ -35,7 +37,7 @@ YoutubeStreamer.prototype.seek = function(start, end) {
 	var self = this;
 	start = start || 0;
 
-	this._video = ytdl(this._source, {quality: this._options.hd ? 22 : 18, range: start + '-' + (end !== undefined ? end : '')});
+	this._video = ytdl(this._source, {quality: this._options.youtube.audio ? 140 : (this._options.youtube.hd ? 22 : 18), range: start + '-' + (end !== undefined ? end : '')});
 	this._video.on('info', function(info, format) {
 		self._progress.setLength(format.size);
 	})
@@ -50,6 +52,7 @@ YoutubeStreamer.prototype.destroy = function() {
 	this._streamify.unresolve();
 	this._video = null;
 	this._destroyed = true;
+    this.file = {};
 }
 
 module.exports = YoutubeStreamer;
