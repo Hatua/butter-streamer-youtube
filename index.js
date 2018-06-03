120002f8f23379c7e4eae97e98b7966ac8acf16d
Niv Sardi <xaiki@evilgiggle.com>
2018-06-03 01:31:50 -0300

    port to 0.1.0 API
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const ytdl = require('ytdl-core')
 const debug = require('debug')('butter-streamer-youtube')
 
-const Streamer = require('butter-base-streamer')
+const Streamer = require('butter-streamer')
 const config = {
   name: 'YouTube Streamer',
   domain: /(youtu.be|youtube.com)/,
@@ -9,11 +9,21 @@ const config = {
   type: 'youtube',
   priority: 10
 }
-/* -- YouTube Streamer -- */
+
+const processVideoInfo = (info, format) => ({
+  length: processLength(info, format),
+  type: format.type,
+  name: 'youtube-video.' + format.container
+})
+
+const processLength = (info, format) => {
+  const bitrate = format.bitrate.split('-').pop()*150000 + format.audioBitrate*150
+  return info.length_seconds * bitrate
+}
+
 class YoutubeStreamer extends Streamer {
-  constructor (source, options) {
+  constructor (source, options = {}) {
     super(options)
-    options = options || {}
     options.youtube = options.youtube || {}
 
     this.config = config
@@ -22,16 +32,8 @@ class YoutubeStreamer extends Streamer {
     this._source = source
     this.file = {}
     this._video = ytdl(source, {quality: options.youtube.audio ? 140 : (options.youtube.hd ? 22 : 18)})
-    this._video.on('info', (info, format) => {
-      debug('info', info, format)
-      this._progress.setLength(format.size)
-      this._isReady({
-        length: info.timestamp * format.bitrate,
-        type: format.type,
-        name: 'youtube-video.' + format.container
-      })
-    })
-    this._streamify.resolve(this._video)
+    this._video.on('info', (info, format) =>
+      this.ready(this._video, processVideoInfo(info, format)))
   }
 
   seek (start, end) {
@@ -40,17 +42,16 @@ class YoutubeStreamer extends Streamer {
     start = start || 0
 
     this._video = ytdl(this._source, {quality: this._options.youtube.audio ? 140 : (this._options.youtube.hd ? 22 : 18), range: start + '-' + (end !== undefined ? end : '')})
-    this._video.on('info', (info, format) => {
-      this._progress.setLength(format.size)
-    })
 
-    this._streamify.unresolve()
-    this._streamify.resolve(this._video)
+    this._video.on('info', (info, format) =>
+      this.reset(this._video, processVideoInfo(info, format))
+    )
   }
+
   destroy () {
     if (this._destroyed) throw new ReferenceError('Streamer already destroyed')
 
-    this._streamify.unresolve()
+    this.close()
     this._video = null
     this._destroyed = true
     this.file = {}
