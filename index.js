f480333946e79a43c89a305639289f98c3f96057
Niv Sardi <xaiki@evilgiggle.com>
2018-06-04 11:22:16 -0300

    working (but lacking tests) webtorrent-server version
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const ytdl = require('ytdl-core')
 const debug = require('debug')('butter-streamer-youtube')
 
+const EventEmitter = require('events').EventEmitter
 const Streamer = require('butter-streamer')
 const config = {
   name: 'YouTube Streamer',
@@ -21,25 +22,54 @@ const processLength = (info, format) => {
   return info.length_seconds * bitrate
 }
 
+class YoutubeFile extends EventEmitter{
+  constructor(info, format) {
+    super()
+
+    Object.assign(this, info)
+    this.name = format.type
+    this.fid = format.itag
+    this.path = format.type
+    this.length = format.clen
+    this.ready = false
+    this.stream = null
+
+    debug('created', format)
+  }
+
+  createReadStream() {
+    this.stream = ytdl(this.video_url, {quality: Number(this.fid)})
+      .on('info', (info, format) => {
+        this.streamReady()
+      })
+
+    return this.stream
+  }
+
+  streamReady() {
+    this.ready = true
+
+    debug('ready', this.format)
+    this.emit('ready', this.stream)
+  }
+}
+
 class YoutubeStreamer extends Streamer {
   constructor (source, options) {
     options.youtube = options.youtube || {}
     super(source, options, config)
   }
 
-  createStream(source, opts) {
-    return new Promise((accept, reject) => {
-      this._video = ytdl(source, {
-        quality: this.options.youtube.audio ? 140 : (this.options.youtube.hd ? 22 : 18),
-        range: opts ? opts.start + '-' + (opts.end !== undefined ? opts.end : ''): undefined
-      })
+  initialize(source, options, config) {
+    return new Promise((resolve, reject) => {
+      this._video = ytdl(source)
 
-      this._video.on('info', (info, format) =>
-        accept({
-          stream: this._video,
-          file: processFileInfo(info, format)
-        })
-      )
+      this._video.on('info', (info, format) => {
+        this.name = info.title
+        const files = info.formats.map(format => new YoutubeFile(info, format))
+
+        resolve(files)
+      })
     })
   }
 
